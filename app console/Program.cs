2f972c2d44c899cfc5f30b0430e4f8a1019a568c
HugoWeb23 <hugohourriez@live.be>
2020-09-19 23:22:40 +0200

    First commit

---
+++ app console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace app_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] tableau = new String[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi" };
+            foreach(String arg in tableau)
+            {
+                Console.WriteLine(arg);
+            }
+
+        }
+    }
+}
