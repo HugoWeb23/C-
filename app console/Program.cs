4c49706aa74ca0635c788044e8bf37bfbd726cea
HugoWeb23 <hugohourriez@live.be>
2020-09-21 18:13:07 +0200

    Hypo

--- app console/Program.cs
+++ app console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace app_console
 {
@@ -6,11 +7,15 @@ namespace app_console
     {
         static void Main(string[] args)
         {
-            String[] tableau = new String[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi" };
-            foreach(String arg in tableau)
-            {
-                Console.WriteLine(arg);
-            }
+            Console.WriteLine("Entrez la veleur du côté A");
+            String CA = Console.ReadLine();
+            Console.WriteLine("Entrez la veur du côté B");
+            String CB = Console.ReadLine();
+            int A = int.Parse(CA);
+            int B = int.Parse(CB);
+            int resultat = (A * A) + (B * B);
+            Console.WriteLine("Hypo = {0}", resultat);
+
 
         }
     }
