e4867d736e65daaafc132084ba3c4c52111268f8
HugoWeb23 <hugohourriez@live.be>
2020-10-07 20:19:59 +0200

    avancement

--- app console/Program.cs
+++ app console/Program.cs
@@ -12,8 +12,77 @@ namespace app_console
     {
         static void Main()
         {
-            
+            /*
+            // Exercice A
 
+            int nombre;
+            Console.WriteLine("Veuillez saisir un nombre");
+            if(int.TryParse(Console.ReadLine(), out nombre) == false)
+            {
+                Console.WriteLine("Saisie invalide !");
+            } else {
+            if(nombre % 3 == 0)
+            {
+                Console.WriteLine("Ce nombre est divisible par 3");
+            } else
+            {
+                Console.WriteLine("Ce nombre n'est pas divisible par 3");
+            }
+            }
+            */
+
+            // Exercice B
+
+            /*
+            int nombre;
+            bool termine = false;
+                while (termine == false)
+                {
+                    Console.WriteLine("Veuillez saisir un nombre");
+                    if (int.TryParse(Console.ReadLine(), out nombre) == false)
+                    {
+                    Console.WriteLine("Saisie invalide !");
+                    break;
+                    }
+                if (nombre < 10)
+                    {
+                        Console.WriteLine("Plus grand !");
+                    }
+                    if (nombre > 20)
+                    {
+                        Console.WriteLine("Plus petit !");
+                    }
+                    if(nombre >= 10 && nombre <= 20)
+                     {
+                    termine = true;
+                    break;
+                      }
+                    }
+
+            */
+            int nombre;
+            Console.WriteLine("Veuillez saisir un nombre");
+            if (int.TryParse(Console.ReadLine(), out nombre) == false)
+            {
+                Console.WriteLine("Saisie invalide !");
+            }
+            Console.WriteLine(Somme(nombre));
+
+
+
+        }
+
+        static int Somme(int nombre)
+        {
+            int resultat = 0;
+            for (int i = nombre; i > 1; i--)
+            {
+                int tempo = nombre;
+                tempo = tempo + i--;
+                 resultat = resultat + tempo;
+
+            }
+            return resultat;
         }
 
     }
